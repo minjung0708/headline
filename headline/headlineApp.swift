b1162f3b936acf0cae1dbc10cae06813a2e1bf2b
Kwon-Minjung <kwon.minjung@lotte.net>
2024-10-29 16:11:14 +0900

    Initialize project.

---
+++ headline/headlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  headlineApp.swift
+//  headline
+//
+//  Created by MinJung on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct headlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
